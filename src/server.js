26a4932c420f90bb89c3b714cf4b6cef62973eda
gianfra92 <gianfra92@gmail.com>
2020-10-15 17:25:51 +0200

    added root page

--- src/server.js
+++ src/server.js
@@ -14,6 +14,10 @@ app.post('/login',services.login);
 app.get('/profile',services.tokenControl,services.profile);
 app.get('/list',services.getList);
 
+app.get('/',(req,res)=>{
+    res.send(`Welcome to Mocked Server! Check API at this <a href="https://github.com/gianfra92/mockedbackend.git">link</a> `)
+})
+
 app.listen(PORT,()=>{
     console.log(`Listening on port ${PORT}`);
 })
